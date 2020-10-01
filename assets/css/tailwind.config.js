3ffe32bb76e8f2e2ec328497295c6d8ef116c957
milafrerichs <mila.frerichs@gmail.com>
2020-10-01 17:33:38 +0200

    feat: birth of the new style

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        cv : '#2b4c7e',
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+  ],
+}
